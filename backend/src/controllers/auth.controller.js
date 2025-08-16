eb48b5a1a93d5c82a106cb8424276297b04a9310
mmtabish <80705398+mmtabish@users.noreply.github.com>
2025-08-16 16:56:24 +0530

    new

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import cloudinary from "../lib/cloudinary.js";
 import { generateToken } from "../lib/utils.js";
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-export const signup = async (req, res) =>{
+export const signup = async (req, res) =>{ 
     const {fullName, email, password} = req.body;
    try{
     //hash password
